113de33528503a3e2fa5992441bdcf6aff59c1b5
Aidan-Killeen <killeeai@tcd.ie>
2024-03-01 15:52:00 +0000

    Feature reduction code is now a function. Using it for 20 features resulted in a 1% accuracy reduction for Ridge regression.

--- modules/helper_functions.py
+++ modules/helper_functions.py
@@ -2,7 +2,7 @@
 Helper function for use in notebooks
 
 """
-
+import pandas as pd
 """_summary_
   df: dataframe
   limit: value between 0 and 1
@@ -16,3 +16,25 @@ def count_nan_features(df, limit):
     if count > limitCount:
       nanCountTotalFeatures += 1
   print(f"No of features with more than {round(limitCount*100/len(df),1)}% NaN's:", nanCountTotalFeatures)
+
+NO_FEATURES_KEPT = 20
+
+
+
+def feature_reduction_x(df):
+  X = df.drop(["BerRating", "EnergyRating"], axis='columns')
+  y1 = df.BerRating
+  y2 = df.EnergyRating
+
+  X = pd.get_dummies(X)
+  values = X.corrwith(y1).abs()
+  factors = values.nlargest(NO_FEATURES_KEPT).keys()
+  return df[factors]
+
+  #print(X.corrwith(y))
+
+
+if __name__ == "__main__":
+  df = pd.read_csv("../data/training/BERRatingData_aa.csv", sep=";", on_bad_lines="skip", low_memory=False)
+  df = feature_reduction(df, 10)
+  print(df.keys())
