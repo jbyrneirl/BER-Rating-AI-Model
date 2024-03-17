eafeaeabc93d52e5a67582000a45db93f8052d24
Aidan-Killeen <killeeai@tcd.ie>
2024-03-17 18:23:06 +0000

    Altered Feature reduction - has extra methodology, but it takes longer and is less accurate (could be improved)

--- modules/helper_functions.py
+++ modules/helper_functions.py
@@ -8,8 +8,13 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 
 from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import SelectFromModel
 from sklearn.feature_selection import f_classif
 from sklearn.impute import SimpleImputer
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import confusion_matrix
 
 """_summary_
   df: dataframe
@@ -30,6 +35,12 @@ NO_FEATURES_KEPT = 100
 
 
 def feature_reduction_x(df):
+  #Can just alter this for quick change
+  #return feature_reduction_corr(df)  #.4848 for decision tree   #.4168 for ridge regression  
+  return feature_reduction_kBest(df)  #.4896 for decision tree  #.4252 for ridge regression
+  #return feature_reduction_decision_tree(df)  #.41 for ridge regression - took a long time
+
+def feature_reduction_corr(df):
   #convert to X
   X = df.drop(["BerRating", "EnergyRating"], axis='columns')
   X = X.drop(['CPC', 'EPC', 'RER', 'RenewEPnren', 'RenewEPren', 'SA_Code', 'PurposeOfRating', 'HESSchemeUpgrade', 'DateOfAssessment', 'CO2Rating', 'CO2MainSpace', 'MPCDERValue'], axis='columns')
@@ -49,7 +60,7 @@ def feature_reduction_x(df):
   return X[factors]
 
 
-def feature_reduction_x2(df):
+def feature_reduction_kBest(df):
   X = df.drop(["BerRating", "EnergyRating"], axis='columns')
   X = X.drop(['CPC', 'EPC', 'RER', 'RenewEPnren', 'RenewEPren', 'SA_Code', 'PurposeOfRating', 'HESSchemeUpgrade', 'DateOfAssessment', 'CO2Rating', 'CO2MainSpace', 'MPCDERValue'], axis='columns')
 
@@ -58,17 +69,48 @@ def feature_reduction_x2(df):
 
   X = pd.get_dummies(X)
 
-  imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+  #imp = SimpleImputer(missing_values=np.nan, strategy='constant')
+  imp = SimpleImputer(missing_values=np.nan, strategy='mean')  #.43
   imp.fit(X)
-  X = imp.transform(X)
+  #X = imp.transform(X)
+  X = pd.DataFrame(imp.fit_transform(X), columns = imp.get_feature_names_out())
 
-  #scaler = MinMaxScaler()
-  #Xnp = scaler.fit_transform(Xt)
-  #X = pd.DataFrame(Xnp, index=X.index, columns=X.columns)
+  scaler = MinMaxScaler()
+  # X = scaler.fit_transform(X)
+  X = pd.DataFrame(scaler.fit_transform(X), columns = imp.get_feature_names_out())
 
-  
+  res_mod = SelectKBest(f_classif, k=NO_FEATURES_KEPT).set_output(transform="pandas")
+  res = res_mod.fit_transform(X, y2)
+  return res
 
-  return SelectKBest(f_classif, k=NO_FEATURES_KEPT).fit_transform(X, y2)
+def feature_reduction_decision_tree(df):
+  X = df.drop(["BerRating", "EnergyRating"], axis='columns')
+  X = X.drop(['CPC', 'EPC', 'RER', 'RenewEPnren', 'RenewEPren', 'SA_Code', 'PurposeOfRating', 'HESSchemeUpgrade', 'DateOfAssessment', 'CO2Rating', 'CO2MainSpace', 'MPCDERValue'], axis='columns')
+
+  y1 = df.BerRating
+  y2 = df.EnergyRating
+
+  X = pd.get_dummies(X)
+
+  X_train, X_test, y_train, y_test = train_test_split( X, y2, stratify=y2, random_state=2)
+  best_score = 0
+  best_depth = 0
+
+  for d in range(2,15):
+      model = DecisionTreeClassifier(max_depth=d)
+      scores = cross_val_score(model, X_train, y_train, cv=5)
+      mean = scores.mean()
+      #print("Depth: ", d, "Accuracy:", mean)
+      if(mean > best_score):
+          best_score = mean
+          best_depth = d
+
+  model = DecisionTreeClassifier(max_depth=best_depth)
+  model.fit(X_train,y_train)
+
+  feature_model = SelectFromModel(model, prefit=True)
+
+  return feature_model.transform(X)
 
 
 
